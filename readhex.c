c64658162fb87d214ec7d78ed058d9c12c1a644b
root <root@coldfusion.(none)>
2018-02-02 16:11:39 -0700

    Update to readhex for command line argument options.

--- readhex.c
+++ readhex.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(void)
+void main(int argc, char** argv)
 {
     FILE *fp;
     unsigned char buffer[80];
 
-    fp=fopen("/dev/ttyUSB0", "r");
+    if(argc < 2)
+    {
+        printf("Will use ttyUSB0\n");
+        fp=fopen("/dev/ttyUSB0", "r");
+    }
+    else
+    {
+        printf("Will use %s\n", argv[1]);
+        fp=fopen(argv[1], "r");
+    }
 
     if(fp == (FILE *)0)
     {
-        perror("opening ttyUSB0");  
+        perror("opening device or ttyUSB0");  
         exit(-1);
     }
    
